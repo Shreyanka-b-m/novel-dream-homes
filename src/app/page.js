3677393aa8601a8c0faf125b5065f84d3de84bc6
Shreyanka-b-m <shreyankabm@gmail.com>
2025-10-22 19:55:30 +0530

    feat: add hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,27 @@
+// app/page.js
 import Image from "next/image";
+import "./globals.css";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className={styles.page}>
-      <main>
-        <h1>Welcome to Novel Dream Homes!!</h1>
-      </main>
-    </div>
+    <main>
+      <section className={`${styles.hero} container`}>
+        <Image
+          src="/images/hero-banner.png"
+          alt="Modern house"
+          fill
+          // width={900}
+          // height={400}
+          priority
+          className={styles.heroImage}
+        />
+        {/* <div className={styles.overlay}></div> */}
+        <div className={styles.heroContent}>
+          <h1>THE KEY TO YOUR FUTURE STARTS HERE</h1>
+          <button className={styles.btn}>Finding Your Home</button>
+        </div>
+      </section>
+    </main>
   );
 }
