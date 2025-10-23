b92f6fb34944202722a8c62b62efec75baaca429
Shreyanka-b-m <shreyankabm@gmail.com>
2025-10-23 17:04:56 +0530

    feat: add subscribe to newsletter section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 // app/page.js
 import Image from "next/image";
+import ContactForm from "@/components/ContactForm/ContactForm";
 import "./globals.css";
 import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
-    <main>
-      <section className="container">
+    <div>
+      {/********* Hero Section Starts *********/}
+      <main className="container">
         <div className={styles.heroWrapper}>
           <Image
             src="/images/hero-banner.png"
@@ -17,13 +19,28 @@ export default function HomePage() {
           />
           <div className={styles.heroOverlay}></div>
           <div className={styles.heroContent}>
-            <h1>
-              THE KEY TO YOUR FUTURE STARTS HERE
-            </h1>
+            <h1>THE KEY TO YOUR FUTURE STARTS HERE</h1>
             <button>Finding Your Home</button>
           </div>
         </div>
+      </main>
+      {/********* Hero Section Ends *********/}
+
+      {/********* Subscribe to Newsletter Section Starts *********/}
+      <section className={`container ${styles.newsletterSection}`}>
+        <div className={styles.newsletterFlexItem}>
+          <h2>subscribe to our NEwsletter</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Posuere ornare quis morbi
+            consectetur et. Interdum lectus in massa turpis.
+          </p>
+        </div>
+
+        <div className={styles.newsletterFlexItem}>
+          <ContactForm />
+        </div>
       </section>
-    </main>
+      {/********* Subscribe to Newsletter Section Ends *********/}
+    </div>
   );
 }
