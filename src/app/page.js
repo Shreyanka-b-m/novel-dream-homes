a9f5f127720e6cb893d3214f00253ec79c027404
Shreyanka-b-m <shreyankabm@gmail.com>
2025-10-23 12:11:32 +0530

    refactor: modify home page hero section

--- src/app/page.js
+++ src/app/page.js
@@ -6,20 +6,22 @@ import styles from "./page.module.css";
 export default function HomePage() {
   return (
     <main>
-      <section className={`${styles.hero} container`}>
-        <Image
-          src="/images/hero-banner.png"
-          alt="Modern house"
-          fill
-          // width={900}
-          // height={400}
-          priority
-          className={styles.heroImage}
-        />
-        {/* <div className={styles.overlay}></div> */}
-        <div className={styles.heroContent}>
-          <h1>THE KEY TO YOUR FUTURE STARTS HERE</h1>
-          <button className={styles.btn}>Finding Your Home</button>
+      <section className="container">
+        <div className={styles.heroWrapper}>
+          <Image
+            src="/images/hero-banner.png"
+            alt="Modern house exterior"
+            fill
+            priority
+            className={styles.heroImage}
+          />
+          <div className={styles.heroOverlay}></div>
+          <div className={styles.heroContent}>
+            <h1>
+              THE KEY TO YOUR FUTURE STARTS HERE
+            </h1>
+            <button>Finding Your Home</button>
+          </div>
         </div>
       </section>
     </main>
