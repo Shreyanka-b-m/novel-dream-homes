8eb20fbe5059ac2eccd1056422f97dd2d221ae6b
Shreyanka-b-m <shreyankabm@gmail.com>
2025-10-24 15:16:00 +0530

    feat: add sample properties data object

---
+++ src/data/properties.js
@@ -0,0 +1,29 @@
+// Array of property objects
+const properties = [
+  {
+    id: 1,
+    name: "Ocean View Apartment",
+    address: "123 Beach Road, Miami",
+    lat: 25.7617,
+    lng: -80.1918,
+    price: "$500,000",
+  },
+  {
+    id: 2,
+    name: "City Center Condo",
+    address: "456 Downtown St, New York",
+    lat: 40.7128,
+    lng: -74.006,
+    price: "$750,000",
+  },
+  {
+    id: 3,
+    name: "Suburban House",
+    address: "789 Green Ave, Dallas",
+    lat: 32.7767,
+    lng: -96.797,
+    price: "$400,000",
+  },
+];
+
+export default properties;
